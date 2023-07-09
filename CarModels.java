95b8310833857bf45ed682c52c3f4736f3729119
sureshkumarreddy-k <112793655+sureshkumarreddy-k@users.noreply.github.com>
2023-07-09 21:22:13 +0530

    Update and rename project1.java to CarModels.java

---
+++ CarModels.java
@@ -0,0 +1,12 @@
+
+public class CarModels {
+	//public static void main(String args[]) {
+		public void model() {
+			String[] model= {"Polo Trendline","Polo Highline ","Virtus Trendline ","Virtus Highline","Taigun Trendline ","Taigun Highline","Taigun Topline"};
+			System.out.println("Car Models");
+			for(int i=0;i<model.length;i++) {
+				System.out.println(model[i]);
+			}
+			
+		}
+}
